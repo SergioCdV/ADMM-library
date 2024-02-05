774ab512733181f37ceab0a163f1b53d67826844
SergioCdV <66524268+SergioCdV@users.noreply.github.com>
2024-02-05 20:30:06 +0100

    <update>: pre-conditioning on L2

--- +Potter_paper_2024/HWC_example.m
+++ +Potter_paper_2024/HWC_example.m
@@ -68,7 +68,7 @@ myMission = LinearMission(nu, Phi, B, x0, xf, K);       % Mission
 dVmin = 0;                                              % Minimum control authority
 dVmax = Inf;                                             % Maximum control authority
 
-myThruster = thruster('L1', dVmin / Vc, dVmax / Vc);
+myThruster = thruster('L2', dVmin / Vc, dVmax / Vc);
 
 %% Optimization
 % Define the ADMM problem 
